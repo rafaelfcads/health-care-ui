8212083b2915e8ae1a8b4b6a193b4dfa16a162af
Rafael Reis <rafaelfc.ads@gmail.com>
2019-01-09 23:06:34 -0200

    Route implementation

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Home from './components/Home.vue'
 import store from './store'
+import router from './routers';
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   store,
   render: h => h(Home),
 }).$mount('#app')
